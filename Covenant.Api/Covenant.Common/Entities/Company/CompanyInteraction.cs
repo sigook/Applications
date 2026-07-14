799de7c00d0d9e95adfb707c98e794df6ac03a87
mancourt69 <juanm@sigook.com>
2026-07-14 10:13:06 -0500

    Interaction's entity, repo, configuration and dbSet added

---
+++ Covenant.Api/Covenant.Common/Entities/Company/CompanyInteraction.cs
@@ -0,0 +1,49 @@
+﻿namespace Covenant.Common.Entities.Company;
+
+public class CompanyInteraction
+{
+    public CompanyInteraction() { }
+
+    public CompanyInteraction(string description, User owner, CompanyProfile company, Purpose purpose, Type type, Status status)
+    {
+        Id = Guid.NewGuid();
+        Description = description;
+        Owner = owner;
+        Company = company;
+        InteractionPurpose = purpose;
+        InteractionType = type;
+        InteractionStatus = status;
+    }
+
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public User Owner { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set;} = DateTime.UtcNow;
+    public CompanyProfile Company { get; set; }
+    public Purpose InteractionPurpose { get; set; }
+    public Type InteractionType { get; set; }
+    public Status InteractionStatus { get; set; } = Status.NotStarted;
+
+    public enum Purpose
+    {
+        Intro = 0,
+        FollowUp = 1,
+        Proposal = 2,
+        Negotiation = 3,
+        Closing = 4
+    }
+    public enum Type
+    {
+        Call = 0,
+        Mail = 1,
+        Sms = 2,
+        LinkedIn = 3
+    }
+    public enum Status
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        Completed = 2
+    }
+}
---
+++ Covenant.Api/Covenant.Common/Repositories/Company/ICompanyInteractionRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Covenant.Common.Repositories.Company;
+
+public interface ICompanyInteractionRepository
+{
+    Task Create<T>(T entity) where T : class;
+    void Delete<T>(T entity) where T : class;
+    void Update<T>(T entity) where T : class;
+}
