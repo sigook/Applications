7afa8b98c9e437e03e57cf2e58a956d9997fd550
mancourt69 <juanm@sigook.com>
2026-07-15 10:09:09 -0500

    Conflicts resolved

--- Sigook.Web/src/components/Comments.vue
+++ Sigook.Web/src/components/Comments.vue
@@ -41,7 +41,7 @@
 import { ref } from 'vue';
 import { useSecurityStore } from '@/stores/security';
 import { showAlertError } from "@/utils/toast";
-import roles, { hasAnyRole, roleGroups } from "@/security/roles";
+import roles from "@/security/roles";
 import { agencyCommentWorker } from "@/api/agencyWorkerApi";
 import { companyCommentWorker } from "@/api/companyApi";
 import iconAgency from '@/assets/images/icon_agency.svg';
